15bae5b4ea567e4fa1622d4f40f28435c3609f15
Chad Eymard <chad.eymard@coop.org>
2021-01-01 12:57:11 -0500

    securedData model

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Chilkat;
+using decryptor.model;
 
 namespace decryptor
 {
@@ -7,62 +7,29 @@ namespace decryptor
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\n*************   Decryptor Tool   **************\n\n");
+
             bool resume = true;
-
-            Console.WriteLine("Decryptor Tool");
-
-            while (resume) {
-                Console.WriteLine("\nEnter the base64-encoded 256-bit encryption key that was used to encrypt the data (aka: The Wrapping Key):");
-
-                string wrappingKey = Console.ReadLine();
+            while (resume)
+            {
+                SecuredData securedData;
 
                 Console.WriteLine("\nEnter the securedData you want decrypted:");
-                string jwe = Console.ReadLine();
+                securedData.CipherText = Console.ReadLine().ToString();
 
-                string decryptedValue = Decrypt(jwe, wrappingKey);
+                Console.WriteLine("\nEnter the base64-encoded 256-bit encryption key that was used to encrypt the data (aka: The Wrapping Key):");
+                securedData.Base64EncodedAESWrappingKey = Console.ReadLine().ToString();
 
-                Console.WriteLine("\n");
+                Console.WriteLine($"\n{securedData.Decrypt()}");
 
-                Console.WriteLine(decryptedValue);
-
-                Console.WriteLine("\nPress the Enter key to continue, or  type 'x' to exit\n");
+                Console.WriteLine("\nPress the Enter key to continue, or  [Ctrl + c] to exit.\n");
 
                 string response = Console.ReadLine();
 
                 resume = response.ToLower() == "x" ? false : true;
             }
-            
+
         }
 
-        static string Decrypt(string securedData, string base64EncodedAESWrappingKey)
-        {
-            //  First, load the JWE..
-            bool success;
-            Jwe jwe = new Jwe();
-            success = jwe.LoadJwe(securedData);
-            if (success != true)
-            {
-                //Console.WriteLine(jwe2.LastErrorText);
-                //return;
-                return jwe.LastErrorText;
-            }
-
-            //  Set the AES wrap key for the recipient index.
-            // This is the base64 encoded 256-bit encryption key
-            jwe.SetWrappingKey(0, base64EncodedAESWrappingKey, "base64url");
-
-            //  Decrypt
-            string originalPlaintext = jwe.Decrypt(0, "utf-8");
-            if (jwe.LastMethodSuccess != true)
-            {
-                //Console.WriteLine(jwe.LastErrorText);
-                //return;
-                return jwe.LastErrorText;
-            }
-
-            //Console.WriteLine("original text decrypted with AES key wrap key: ");
-            //Console.WriteLine(originalPlaintext);
-            return originalPlaintext;
-        }
     }
 }
---
+++ model/SecuredData.cs
@@ -0,0 +1,36 @@
+﻿using Chilkat;
+
+namespace decryptor.model
+{
+    public struct SecuredData
+    {
+        public string CipherText;
+        public string Base64EncodedAESWrappingKey;
+
+        
+        public string Decrypt()
+        {
+            //  First, load the JWE..
+            bool success;
+            Jwe jwe = new Jwe();
+            success = jwe.LoadJwe(CipherText);
+            if (success != true)
+            {
+                return jwe.LastErrorText;
+            }
+
+            //  Set the AES wrap key for the recipient index.
+            // This is the base64 encoded 256-bit encryption key
+            jwe.SetWrappingKey(0, Base64EncodedAESWrappingKey, "base64url");
+
+            //  Decrypt
+            string clearText = jwe.Decrypt(0, "utf-8");
+            if (jwe.LastMethodSuccess != true)
+            {
+                return jwe.LastErrorText;
+            }
+
+            return clearText;
+        }
+    }
+}
