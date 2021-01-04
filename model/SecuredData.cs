dd70e07189aafbb52d2d440928b6e49c0da86c50
Chad Eymard <chad.eymard@coop.org>
2021-01-03 20:02:32 -0500

    code refactor

--- model/SecuredData.cs
+++ model/SecuredData.cs
@@ -8,27 +8,21 @@ namespace Decryptor.Model
         public static string Base64EncodedAESWrappingKey;       
         public static string Decrypt()
         {
-            //  First, load the JWE..
-            bool success;
+            
             Jwe jwe = new Jwe();
-            success = jwe.LoadJwe(CipherText);
-            if (success != true)
-            {
-                return jwe.LastErrorText;
-            }
+            
+            //  1. Load the JWE..
+            bool success = jwe.LoadJwe(CipherText);
 
-            //  Set the AES wrap key for the recipient index.
-            // This is the base64 encoded 256-bit encryption key
+            if (success != true) return jwe.LastErrorText;
+
+            //  2. Set the AES wrap key for the recipient index.
+            //     This is the base64 encoded 256-bit encryption key
             jwe.SetWrappingKey(0, Base64EncodedAESWrappingKey, "base64url");
 
-            //  Decrypt
-            string clearText = jwe.Decrypt(0, "utf-8");
-            if (jwe.LastMethodSuccess != true)
-            {
-                return jwe.LastErrorText;
-            }
+            //  3. Decrypt
+            return jwe.LastMethodSuccess != true ? jwe.LastErrorText : jwe.Decrypt(0, "utf-8");
 
-            return clearText;
         }
         public static void Clear()
         {
